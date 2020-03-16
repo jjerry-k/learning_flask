5b4e5609c15084f5a9e49afd45992238b19ad40a
jjerry-k <jaeyeol2931@gmail.com>
2020-03-16 17:35:15 +0900

    Update

---
+++ 00/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # return 'Hello Flask'
+    return render_template('index.html') 
+    
+@app.route('/info')
+def info():
+    # return 'Info'
+    return render_template('info.html')
