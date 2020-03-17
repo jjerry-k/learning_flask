dd14c6956d957db7c31cbc1b18f46bb9b4290871
jjerry-k <jaeyeol2931@gmail.com>
2020-03-18 03:42:26 +0900

    Update

---
+++ 02/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # return home.html
+    return render_template('home.html') 
+
+@app.route('/info')
+def info():
+    #return info.html
+    return render_template('info.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
