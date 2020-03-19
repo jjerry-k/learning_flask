0c6d90b33a41c74ac02bffcbc0bb87ac49243cd3
jjerry-k <jaeyeol2931@gmail.com>
2020-03-20 01:46:19 +0900

    Update

---
+++ 04_jinja_template_language/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    name, nick, age, condition_1, condition_2 = "Kim", "Jerry", 28, False, False
+    template_context = dict(name=name, nick=nick, age=age, 
+                            condition_1=condition_1, condition_2=condition_2)
+    return render_template('index.html', **template_context)
+
+if __name__ == "__main__":
+    app.run(debug=True)
